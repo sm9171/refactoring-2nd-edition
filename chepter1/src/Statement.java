40b8681f1a13be0bc7974d975ab6c0f38a74df8d
Sangmin <sm9171@nate.com>
2025-01-15 21:38:41 +0900

    feat: 1.4 statement() 함수 쪼개기

--- chepter1/src/Statement.java
+++ chepter1/src/Statement.java
@@ -1,45 +1,67 @@
 public class Statement {
     public String statement(Invoice invoice, Plays plays) throws Exception {
-        int totalAmount = 0;
-        int volumeCredit = 0;
         StringBuilder result = new StringBuilder(String.format("청구내역 (고객명: %s)\n", invoice.getCustomer()));
         for (Performance performance : invoice.getPerformances()) {
-            Play play = plays.get(performance);
-            int thisAmount = 0;
-
-            switch (play.getType()) {
-                case TRAGEDY:
-                    thisAmount = 40000;
-                    if (performance.getAudience() > 30) {
-                        thisAmount += 1000 * (performance.getAudience() - 30);
-                    }
-                    break;
-                case COMEDY:
-                    thisAmount = 30000;
-                    if (performance.getAudience() > 20) {
-                        thisAmount += 10000 + 500 * (performance.getAudience() - 20);
-                    }
-                    thisAmount += 300 * performance.getAudience();
-                    break;
-                default:
-                    throw new Exception("알 수 없는 장르");
-            }
-
-            // 포인트를 적립한다.
-            volumeCredit += Math.max(performance.getAudience() - 30, 0);
-
-            // 희극 관객 5명마다 추가 포인트를 제공핟나.
-            if (play.getType().equals(PlayType.COMEDY)) {
-                volumeCredit += Math.floor(performance.getAudience() / 5);
-            }
-
-            // 청구 내역을 출력한다.
-            result.append(String.format("%s: $%d %d석\n",play.getName(), thisAmount / 100, performance.getAudience()));
-            totalAmount += thisAmount;
+            result.append(String.format("%s: $%d %d석\n",playFor(plays,performance).getName(), amountFor(performance, plays) / 100, performance.getAudience()));
         }
 
-        result.append(String.format("총액: $%d\n",totalAmount / 100));
-        result.append(String.format("적립 포인트: %d점", volumeCredit));
+        result.append(String.format("총액: $%d\n",totalAmount(invoice, plays, result) / 100));
+        result.append(String.format("적립 포인트: %d점", totalVolumeCredits(invoice, plays)));
         return result.toString();
+    }
+
+    private static int totalAmount(final Invoice invoice, final Plays plays, final StringBuilder result) throws Exception {
+        int totalAmount = 0;
+        for (Performance performance : invoice.getPerformances()) {
+            totalAmount += amountFor(performance, plays);
+        }
+        return totalAmount;
+    }
+
+    private static int totalVolumeCredits(final Invoice invoice, final Plays plays) {
+        int volumeCredit = 0;
+        for (Performance performance : invoice.getPerformances()) {
+            volumeCredit = volumeCreditFor(plays, performance);
+        }
+        return volumeCredit;
+    }
+
+    private static int volumeCreditFor(final Plays plays, final Performance performance) {
+        int volumeCredit = 0;
+
+        // 포인트를 적립한다.
+        volumeCredit += Math.max(performance.getAudience() - 30, 0);
+
+        // 희극 관객 5명마다 추가 포인트를 제공한다.
+        if (playFor(plays, performance).equals(PlayType.COMEDY)) {
+            volumeCredit += Math.floor(performance.getAudience() / 5);
+        }
+        return volumeCredit;
+    }
+
+    private static int amountFor(final Performance ePerformance, final Plays plays) throws Exception {
+        int result = 0;
+        switch (playFor(plays, ePerformance).getType()) {
+            case TRAGEDY:
+                result = 40000;
+                if (ePerformance.getAudience() > 30) {
+                    result += 1000 * (ePerformance.getAudience() - 30);
+                }
+                break;
+            case COMEDY:
+                result = 30000;
+                if (ePerformance.getAudience() > 20) {
+                    result += 10000 + 500 * (ePerformance.getAudience() - 20);
+                }
+                result += 300 * ePerformance.getAudience();
+                break;
+            default:
+                throw new Exception("알 수 없는 장르");
+        }
+        return result;
+    }
+
+    private static Play playFor(Plays plays, Performance performance) {
+        return plays.get(performance);
     }
 }
